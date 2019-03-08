2801e4f564980b87e15363ab321c13b76380affa
Archies11 <45824817+Archies11@users.noreply.github.com>
2019-03-09 03:48:12 +0530

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,96 +1,88 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css?family=Megrim" rel="stylesheet">
+    <title>checkmate</title>
+    <style>
 
-        <title>Laravel</title>
+        body{
+            background-image: url(back1.png);
+            background-size: cover;
+            font-family: 'source sans pro', sans-serif;
+        }
+        header{
+            color:#000066 ;
+            background-color: 	#e4e6e7;
+            margin-top: -20px;
+            margin-left:-10px;
+            margin-right:-8px;
+            padding:20px;
+            padding-right:40px;
+            height: 50px;
+            opacity: 0.85;
+            box-shadow:0px 5px 5px #333333;
+        }
+        .t1{
+            position:absolute;
+            top:37%;
+            left: 28%;
+            font-size:100px;
+            color:	#e4e6e7;
+            font-family:'Megrim';
+            text-shadow: 3px 2px black;
+            letter-spacing: 15px;
+        }
+        .link{
+          float:right;
+          color: #000066;
+          position: relative;
+          text-decoration:none;
+          padding-right:40px;
+          padding-left:40px;
+          overflow:hidden;
+          padding-top:20px;
+          padding-bottom:20px;
+          font-size:20px;
+          display: block;
+        }
+        header a:hover{
+          background-color:#002756;
+          color: #e4e6e7;
+        }
+        .logo{
+          position:relative;
+          float:left;
+          left:0px;
+          width:70px;
+          height:auto;
+        }
+    </style>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+</head>
+<body>
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+    <header>
+        @if (Route::has('login'))
+        
+            @auth
+                <a class="link" href="{{ url('/home') }}">Home</a>
+            @else
+                <a class="link" href="{{ route('login') }}">Login</a>
+                <a class="link" href="{{ route('register') }}">Register</a>
+            @endauth
+        
+    @endif
 
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
-        </div>
-    </body>
+      <!--<a class="link" href="">Register</a>
+      <a class="link" href="">Login</a>-->
+      <a class="link" href="">Documentation</a>
+      <a class="link" href="">About</a>
+      <img class="logo" src="logo_that_shall_not_be_named.png">
+    </header>
+    <h1 class="t1">CHECKMATE</h1>
+</body>
 </html>
+
